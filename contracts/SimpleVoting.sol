e4afadcf03e4d7e4d37c52adc5101a4d17deadf1
Anil Kumar Behera <ark845612@gmail.com>
2024-06-16 20:46:35 +0530

    tally, events, docs added

--- contracts/SimpleVoting.sol
+++ contracts/SimpleVoting.sol
@@ -15,6 +15,15 @@ contract SimpleVoting{
     mapping(uint =>mapping(uint=>uint)) private _tally;
     mapping(uint=> mapping(address=>bool)) public hasVoted;
 
+    event BallotCreated(uint indexed ballotIndex, string question , string[] options, uint startTime, uint duration);
+    event VoteCast(uint indexed ballotIndex,uint indexed optionIndex, address indexed voter);
+
+    /// @notice Creates a new ballot
+    /// @param question_ The question for the ballot
+    /// @param options_ The options for the ballot
+    /// @param startTime_ The start time for the ballot (in seconds since Unix epoch)
+    /// @param duration_ The duration for which the ballot is active (in seconds)
+
     function createBallot(
         string memory question_,
         string[] memory options_,
@@ -25,13 +34,24 @@ contract SimpleVoting{
         require(startTime_ > block.timestamp, "Start time must be in the future");
         require(duration_>0,"Duration must be greater than 0");
         _ballots[counter]=Ballot(question_,options_,startTime_,duration_);
+        emit BallotCreated(counter, question_, options_, startTime_, duration_);
         counter++;
     }
 
+    /// @notice Retrieves a ballot by its index
+    /// @param index_ The index of the ballot
+    /// @return ballot
     function getBallotByIndex(uint index_) external view returns(Ballot memory ballot){
-        ballot = _ballots[index_];
+        return _ballots[index_];
     }
 
+    function getNumberofBallots() public view returns(uint){
+        return counter;
+    }
+
+    /// @notice Casts a vote on a ballot
+    /// @param ballotIndex_ The index of the ballot
+    /// @param optionIndex_ The index of the option to vote for
     function cast(uint ballotIndex_, uint optionIndex_) external {
         require(!hasVoted[ballotIndex_][msg.sender],"Address already casted a vote for ballot");
         Ballot memory ballot = _ballots[ballotIndex_];
@@ -39,9 +59,49 @@ contract SimpleVoting{
         require(block.timestamp<= ballot.startTime+ballot.duration,"Can't cast after end time");
         _tally[ballotIndex_][optionIndex_]++;
         hasVoted[ballotIndex_][msg.sender]=true;
+        emit VoteCast(ballotIndex_, optionIndex_, msg.sender);
     }
 
+    /// @notice retrive tally for a specific option in a ballot
+    /// @param ballotIndex_ the index of the ballot
+    /// @param optionIndex_ The index of the option
+    /// @return The tally for the option
     function getTally(uint ballotIndex_,uint optionIndex_) external view returns(uint){
         return _tally[ballotIndex_][optionIndex_];
+    }
+    
+    /// @notice Retrieves the results for a ballot
+    /// @param ballotIndex_ The index of the ballot
+    /// @return The results array
+    function results(uint ballotIndex_) external view returns(uint[] memory ){
+        Ballot memory ballot = _ballots[ballotIndex_];
+        uint len= ballot.options.length;
+        uint[] memory result = new uint[](len);
+        for(uint i=0;i<len;i++){
+            result[i]= _tally[ballotIndex_][i];
+        }
+        return result;
+    }
+
+    /// @notice Retrieves the winners of a ballot
+    /// @param ballotIndex_ The index of the ballot
+    /// @return The winners array
+    function winners(uint ballotIndex_) public view returns(bool[] memory){
+        Ballot memory ballot= _ballots[ballotIndex_];
+        uint len= ballot.options.length;
+        uint max;
+        for(uint i=0;i<len;i++){
+            uint votes= _tally[ballotIndex_][i];
+            if(votes>max){
+                max= votes;
+            }
+        }
+        bool[] memory winner = new bool[](len);
+        for(uint i=0;i<len;i++){
+            if(_tally[ballotIndex_][i]==max){
+                winner[i]=true;
+            }
+        }
+        return winner;
     }
 }
--- test/SimpleVoting.ts
+++ test/SimpleVoting.ts
@@ -2,7 +2,9 @@ import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { BigNumber, Contract } from "ethers";
+import { Contract } from "ethers";
+
+
 
 describe("SimpleVoting", function () {
     async function deploy() {
@@ -73,14 +75,12 @@ describe("SimpleVoting", function () {
                 question, options, startTime, duration
             )).to.be.revertedWith("Duration must be greater than 0")
         })
-    });
-    describe("Casting a vote", function () {
-        let contract: Contract;
-        const duration = 300;
 
-        beforeEach(async () => {
-            const fixture = { contract } = await loadFixture(deploy)
-            const startTime = await time.latest() + 60;
+
+        it("should return the number of ballots", async () => {
+            const { contract } = await loadFixture(deploy)
+            const startTime = await time.latest() + 60
+            const duration = 300
             const question = "Who is the greatest rapper of all time?"
             const options = [
                 "Tupac Shakur",
@@ -89,25 +89,87 @@ describe("SimpleVoting", function () {
                 "Jay-Z"
             ]
             await contract.createBallot(question, options, startTime, duration)
+            expect(await contract.getNumberofBallots()).to.be.equal(1);
         })
-        it("should be able to vote", async () => {
-            const [signer] = await ethers.getSigners()
-            await time.increase(61)
-            await contract.cast(0, 0)
-            expect(await contract.hasVoted(0, signer.address)).to.eq(true)
-            expect(await contract.getTally(0, 0)).to.be.eq(1)
+
+
+        describe("Casting a vote", function () {
+            let contract: Contract;
+            const duration = 300;
+
+            beforeEach(async () => {
+                const fixture = { contract } = await loadFixture(deploy)
+                const startTime = await time.latest() + 60;
+                const question = "Who is the greatest rapper of all time?"
+                const options = [
+                    "Tupac Shakur",
+                    "The Notorious B.I.G.",
+                    "Eminem",
+                    "Jay-Z"
+                ]
+                await contract.createBallot(question, options, startTime, duration)
+            })
+            it("should be able to vote", async () => {
+                const [signer] = await ethers.getSigners()
+                await time.increase(61)
+                await contract.cast(0, 0)
+                expect(await contract.hasVoted(0, signer.address)).to.eq(true)
+                expect(await contract.getTally(0, 0)).to.be.eq(1)
+            })
+            it("should revert if the user tries to vote before the start time", async () => {
+                await expect(contract.cast(0, 0)).to.be.revertedWith("Can't cast before start time")
+            })
+            it("should revert if the user tries to vote after the end time", async () => {
+                await time.increase(2000)
+                await expect(contract.cast(0, 0)).to.be.revertedWith("Can't cast after end time")
+            })
+            it("should revert if the user tries to vote multiple times", async () => {
+                await time.increase(61)
+                await contract.cast(0, 0)
+                await expect(contract.cast(0, 1)).to.be.revertedWith("Address already casted a vote for ballot")
+            })
         })
-        it("should revert if the user tries to vote before the start time", async () => {
-            await expect(contract.cast(0, 0)).to.be.revertedWith("Can't cast before start time")
-        })
-        it("should revert if the user tries to vote after the end time", async () => {
-            await time.increase(2000)
-            await expect(contract.cast(0, 0)).to.be.revertedWith("Can't cast after end time")
-        })
-        it("should revert if the user tries to vote multiple times", async () => {
-            await time.increase(61)
-            await contract.cast(0, 0)
-            await expect(contract.cast(0, 1)).to.be.revertedWith("Address already casted a vote for ballot")
+        describe("Tallying votes", async () => {
+            let contract: Contract;
+            const duration = 300;
+            beforeEach(async () => {
+                const fixture = { contract } = await loadFixture(deploy)
+                const startTime = await time.latest() + 60 // start the ballot in 60 seconds
+                const question = "Who is the greatest rapper of all time?"
+                const options = [
+                    "Tupac Shakur",
+                    "The Notorious B.I.G.",
+                    "Eminem",
+                    "Jay-Z"
+                ]
+                await contract.createBallot(
+                    question, options, startTime, duration
+                )
+                await time.increase(61)
+                const signers = await ethers.getSigners()
+
+                await contract.cast(0, 0)
+                await contract.connect(signers[1]).cast(0, 0)
+                await contract.connect(signers[2]).cast(0, 1)
+                await contract.connect(signers[3]).cast(0, 2)
+            })
+
+            it("should return the results for every option", async () => {
+                await time.increase(duration + 1)
+                expect(await contract.results(0)).to.deep.eq([
+                    2n, 1n, 1n, 0n
+                ])
+            })
+            it("should return the winner for a ballot", async () => {
+                await time.increase(duration + 1);
+                expect(await contract.winners(0)).to.deep.eq([true, false, false, false])
+            })
+            it("should return multiple winners for a tied ballot", async () => {
+                const signers = await ethers.getSigners()
+                await contract.connect(signers[4]).cast(0, 2);
+                await time.increase(2000)
+                expect(await contract.winners(0)).to.deep.eq([true, false, true, false]);
+            })
         })
     })
 })
